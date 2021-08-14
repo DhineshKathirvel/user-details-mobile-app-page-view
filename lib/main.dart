18342534a3d88c345ac8e0bd32e73a858e09cc59
dhineshkathirvel <Dhinesh1607>
2021-08-15 04:08:48 +0530

    User Details Screen using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: UserId())
+    );
+
+class UserId extends StatelessWidget {
+  const UserId({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        title: Text("User Profile"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0,40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage("assets/images/Dhinesh-1.jpeg"),
+              ),
+            ),
+            Divider(height: 40.0,
+            color: Colors.grey[600],
+            ),
+            Text("NAME",
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+            ),
+            Text("Dhinesh Kathirvel",
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+                ),
+            ),
+            SizedBox(height: 10.0),
+            Text("COMPANY",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text("HCl Technologies",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text("TITLE",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text("Solution Architect",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width:10.0),
+                Text("dhineshkumar.kathirvel@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
