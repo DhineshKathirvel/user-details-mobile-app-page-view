16da99c5865ca7f2ebb2e3de391af8324691e3da
dhineshkathirvel <Dhinesh1607>
2021-08-19 03:19:58 +0530

    Converted to stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() => runApp(MaterialApp(
     home: UserId())
     );
 
-class UserId extends StatelessWidget {
+class UserId extends StatefulWidget {
   const UserId({Key? key}) : super(key: key);
 
+  @override
+  _UserIdState createState() => _UserIdState();
+}
+
+class _UserIdState extends State<UserId> {
+  int experience=10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +21,15 @@ class UserId extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         centerTitle: true,
         title: Text("User Profile"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+            setState(() {
+              experience +=1;
+            });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0,40.0, 30.0, 0.0),
@@ -67,6 +82,21 @@ class UserId extends StatelessWidget {
               ),
             ),
             Text("Solution Architect",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text("Experience",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text('$experience',
               style: TextStyle(
                   color: Colors.amberAccent,
                   letterSpacing: 2.0,
